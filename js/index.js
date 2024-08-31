a8da4a9e5a083627bb9ddd0d886eee687366cb40
markkizik <deadmark666@gmail.com>
2024-08-31 19:14:11 +0300

    box-generator complete taskkk 31.08.2024

--- js/index.js
+++ js/index.js
@@ -8,7 +8,8 @@ const spread = document.querySelector('.input-spread');
 const inset = document.querySelector('#input-inset');
 const color = document.querySelector('#input-color');
 
-const allInput = document.querySelectorAll('input')
+const allInputFirst = document.querySelectorAll('.box-generator__first input');
+const allInputSecond = document.querySelectorAll('.box-generator__first .box-generato__width input')
 
 const horizontalP = document.querySelector('.label-horizontal');
 const verticalP = document.querySelector('.label-vertical');
@@ -19,25 +20,27 @@ const widthP = document.querySelector('.label-width');
 const width = document.querySelector('.input-width')
 
 const inputChange = () => {
-    const h = horizontal.value;
-    const v = vertical.value;
-    const b = blurInput.value;
-    const s = spread.value;
-    const c = color.value;
-    const w = width.value;
-
-    const insetCheked = inset.checked ? 'inset' : "";
-    block.setAttribute("style", `box-shadow: ${insetCheked} ${h}px ${v}px ${b}px ${s}px ${c}; width: ${w}px;`);
+    block.style.boxShadow= `${inset.checked ? 'inset' : ""} ${horizontal.value}px ${vertical.value}px ${blurInput.value}px ${spread.value}px ${color.value}`;
 
     horizontalP.innerHTML = horizontal.value;
     verticalP.innerHTML = vertical.value;
     blurInputP.innerHTML = blurInput.value;
     spreadP.innerHTML = spread.value;
+};
+
+const widthChange = () => {
+    block.style.width = `${width.value}px`;
     widthP.innerHTML = width.value;
 };
 
-allInput.forEach(element => {
-    element.addEventListener('click', function () {
+allInputFirst.forEach(element => {
+    element.addEventListener('change', function () {
         inputChange();
+    });
+});
+
+allInputSecond.forEach(element => {
+    element.addEventListener('change', function () {
+        widthChange();
     });
 });
