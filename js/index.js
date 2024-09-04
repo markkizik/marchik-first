0d2c93655376cba5dc227c2c9022ebe798f742ef
markkizik <deadmarky666@gmail.com>
2024-09-04 18:10:03 +0000

    [marik 123]

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 const block = document.querySelector('.box-generaror__elm');
 const blockSecond = document.querySelector('.box-generator__first')
 
+const allInputFirst = document.querySelectorAll('.box-generator__first input');
+const allInputSecond = document.querySelectorAll('.box-generator__first .box-generato__width input')
+
 const horizontal = document.querySelector('.input-horizontal');
 const vertical = document.querySelector('.input-vertical');
 const blurInput = document.querySelector('.input-blur');
@@ -8,29 +11,26 @@ const spread = document.querySelector('.input-spread');
 const inset = document.querySelector('#input-inset');
 const color = document.querySelector('#input-color');
 
-const allInputFirst = document.querySelectorAll('.box-generator__first input');
-const allInputSecond = document.querySelectorAll('.box-generator__first .box-generato__width input')
-
-const horizontalP = document.querySelector('.label-horizontal');
-const verticalP = document.querySelector('.label-vertical');
-const blurInputP = document.querySelector('.label-blur');
-const spreadP = document.querySelector('.label-spread');
-const widthP = document.querySelector('.label-width');
+const horizontalNum = document.querySelector('.number-horizontal');
+const verticalNum = document.querySelector('.number-vertical');
+const blurInputNum = document.querySelector('.number-blur');
+const spreadNum = document.querySelector('.number-spread');
+const widthNum = document.querySelector('.number-width');
 
 const width = document.querySelector('.input-width')
 
 const inputChange = () => {
     block.style.boxShadow= `${inset.checked ? 'inset' : ""} ${horizontal.value}px ${vertical.value}px ${blurInput.value}px ${spread.value}px ${color.value}`;
 
-    horizontalP.innerHTML = horizontal.value;
-    verticalP.innerHTML = vertical.value;
-    blurInputP.innerHTML = blurInput.value;
-    spreadP.innerHTML = spread.value;
+    horizontalNum.value = horizontal.value;
+    verticalNum.value = vertical.value;
+    blurInputNum.value = blurInput.value;
+    spreadNum.value = spread.value;
 };
 
 const widthChange = () => {
     block.style.width = `${width.value}px`;
-    widthP.innerHTML = width.value;
+    widthNum.value = width.value;
 };
 
 allInputFirst.forEach(element => {
