ee331f5034b6b98f5e6e53e02384f267a8c50a26
markkizik <deadmark666@gmail.com>
2024-09-08 15:43:15 +0300

    add javascript component [boxshadow] | last commit 08.09.2024

---
+++ js/box-shadow-component.js
@@ -0,0 +1,75 @@
+export const boxShadowComponent = () => {
+
+    const getElement = (name) => {
+        return document.querySelector(name)
+    };
+
+    const block = getElement('.box-generaror__elm');
+    let boxShadow;
+
+    const shadowUpdate = () => {
+        const codes = getElement('.card-code__codes');
+        const horizontal = getElement('.input-horizontal');
+        const vertical = getElement('.input-vertical');
+        const blurInput = getElement('.input-blur');
+        const spread = getElement('.input-spread');
+        const inset = getElement('#input-inset');
+        const color = getElement('#input-color');
+        const shadow = `${inset.checked ? 'inset' : ""} ${horizontal.value}px ${vertical.value}px ${blurInput.value}px ${spread.value}px ${color.value}`;
+
+        block.style.boxShadow = shadow;
+        boxShadow = `box-shadow: ${shadow}`;
+        codes.innerHTML = boxShadow;
+    };
+
+    const widthChange = () => {
+        const width = getElement('.input-width');
+        block.style.width = `${width.value}px`;
+    };
+
+    const buttonCopy = getElement('.copy-button')
+    buttonCopy.addEventListener('click', () => {
+        (!boxShadow) && shadowUpdate();
+        navigator.clipboard.writeText(boxShadow);
+    });
+
+    const setInputValue = (elm, value) => {
+        elm.value = value
+    };
+
+    const initInputs = () => {
+        const allInput = document.querySelectorAll('input');
+
+        allInput.forEach(element => {
+            element.addEventListener('input', function () {
+                const nearElement = element.type != 'range' ? element.nextElementSibling : element.previousElementSibling;
+
+                const isCheckMin = parseInt(element.value) < parseInt(element.min);
+                const isCheckMax = parseInt(element.value) > parseInt(element.max);
+                const isCheckWidth = !element.classList.contains('number-width');
+
+                setInputValue(nearElement, element.value);
+
+                if (isCheckMin && isCheckWidth) {
+                    element.value = element.min;
+                }
+
+                if (isCheckMax) {
+                    element.value = element.max;
+                }
+
+                widthChange();
+                shadowUpdate();
+            });
+
+            element.addEventListener('change', () => {
+                if (!element.value) {
+                    element.value = element.getAttribute('value')
+                }
+            });
+        });
+    };
+
+    initInputs();
+    shadowUpdate();
+}
--- js/index.js
+++ js/index.js
@@ -1,74 +1,10 @@
-const block = document.querySelector('.box-generaror__elm');
-let boxShadow;
-
-const shadowUpdate = () => {
-    const codes = document.querySelector('.card-code__codes');
-    const horizontal = document.querySelector('.input-horizontal');
-    const vertical = document.querySelector('.input-vertical');
-    const blurInput = document.querySelector('.input-blur');
-    const spread = document.querySelector('.input-spread');
-    const inset = document.querySelector('#input-inset');
-    const color = document.querySelector('#input-color');
-    const shadow = `${inset.checked ? 'inset' : ""} ${horizontal.value}px ${vertical.value}px ${blurInput.value}px ${spread.value}px ${color.value}`;
-
-    block.style.boxShadow = shadow;
-    boxShadow = `box-shadow: ${shadow}`;
-    codes.innerHTML = boxShadow;
-};
-
-const widthChange = () => {
-    const width = document.querySelector('.input-width');
-    block.style.width = `${width.value}px`;
-};
-
-function copyText() {
-    (!boxShadow) && shadowUpdate();
-    navigator.clipboard.writeText(boxShadow);
-};
-
-const setInputValue = (elm, value) => {
-    elm.value = value
-};
-
-const initInputs = () => {
-    const allInput = document.querySelectorAll('input');
-
-    allInput.forEach(element => {
-        element.addEventListener('input', function () {
-            const nearElement = element.type != 'range' ? element.nextElementSibling : element.previousElementSibling;
-    
-            const isCheckMin = parseInt(element.value) < parseInt(element.min);
-            const isCheckMax = parseInt(element.value) > parseInt(element.max);
-            const isCheckWidth = !element.classList.contains('number-width');
-    
-            setInputValue(nearElement, element.value);
-    
-            if (isCheckMin && isCheckWidth) {
-                element.value = element.min;
-            }
-    
-            if (isCheckMax) {
-                element.value = element.max;
-            }
-    
-            widthChange();
-            shadowUpdate();
-        });
-    
-        element.addEventListener('change', () => {
-            if (!element.value) {
-                element.value = element.getAttribute('value')
-            }
-        });
-    });
-};
+import { boxShadowComponent } from "./box-shadow-component.js"
 
 const app = () => {
-    initInputs();
-    shadowUpdate();
+    boxShadowComponent();
 }
 
 window.addEventListener('DOMContentLoaded', () => {
     app()
     console.log('✅ Loaded Page');
-  });
+});
